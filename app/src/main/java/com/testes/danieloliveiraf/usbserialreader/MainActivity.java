5f9ffb6ec88a9c196bf50796ab1904d29966afba
Daniel Oliveira <danieldeoliveira1995@gmail.com>
2020-01-30 01:47:55 -0300

    Mostrar localização na tela

--- app/src/main/java/com/testes/danieloliveiraf/usbserialreader/MainActivity.java
+++ app/src/main/java/com/testes/danieloliveiraf/usbserialreader/MainActivity.java
@@ -1,6 +1,10 @@
 package com.testes.danieloliveiraf.usbserialreader;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.location.Location;
+
+
 import android.content.Context;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -8,11 +12,16 @@ import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import android.app.AlertDialog;
 
 import android.os.ParcelFileDescriptor;
 import android.text.method.ScrollingMovementMethod;
@@ -22,6 +31,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -41,8 +57,26 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Set;
 import java.util.Calendar;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity 
+        implements GoogleApiClient.ConnectionCallbacks,
+                   GoogleApiClient.OnConnectionFailedListener, LocationListener{
+
+    /// location variables
+    private Location location;
+    private TextView locationTv;
+    private GoogleApiClient googleApiClient;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private LocationRequest locationRequest;
+    private static final long UPDATE_INTERVAL = 5000, FASTEST_INTERVAL = 5000; // = 5 seconds
+    // lists for permissions
+    private ArrayList<String> permissionsToRequest;
+    private ArrayList<String> permissionsRejected = new ArrayList<>();
+    private ArrayList<String> permissions = new ArrayList<>();
+    // integer for permissions results request
+    private static final int ALL_PERMISSIONS_RESULT = 1011;
+
 
     /*
      * Notifications from UsbService will be received here.
@@ -74,6 +108,41 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "You denied write external storage permission.", Toast.LENGTH_LONG).show();
             }
         }
+
+        switch(requestCode) {
+            case ALL_PERMISSIONS_RESULT:
+                for (String perm : permissionsToRequest) {
+                if (!hasPermission(perm)) {
+                    permissionsRejected.add(perm);
+                }
+                }
+
+                if (permissionsRejected.size() > 0) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                    new AlertDialog.Builder(MainActivity.this).
+                        setMessage("These permissions are mandatory to get your location. You need to allow them.").
+                        setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                requestPermissions(permissionsRejected.
+                                    toArray(new String[permissionsRejected.size()]), ALL_PERMISSIONS_RESULT);
+                            }
+                            }
+                        }).setNegativeButton("Cancel", null).create().show();
+
+                        return;
+                    }
+                }
+                } else {
+                if (googleApiClient != null) {
+                    googleApiClient.connect();
+                }
+            }
+            break;
+        }
+    
     }
 
     private void writeToFile(String data,Context context) {
@@ -169,6 +238,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        locationTv = findViewById(R.id.location);
+        // we add permissions we need to request location of the users
+        permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        permissionsToRequest = permissionsToRequest(permissions);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if (permissionsToRequest.size() > 0) {
+            requestPermissions(permissionsToRequest.toArray(
+                    new String[permissionsToRequest.size()]), ALL_PERMISSIONS_RESULT);
+            }
+        }
+
+        // we build google api client
+        googleApiClient = new GoogleApiClient.Builder(this).
+        addApi(LocationServices.API).
+        addConnectionCallbacks(this).
+        addOnConnectionFailedListener(this).build();
+
         mHandler = new MyHandler(this);
 
         display = (TextView) findViewById(R.id.textView1);
@@ -198,6 +287,10 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         setFilters();  // Start listening notifications from UsbService
         startService(UsbService.class, usbConnection, null); // Start UsbService(if it was not started before) and Bind it
+
+        if (!checkPlayServices()) {
+            locationTv.setText("You need to install Google Play Services to use the App properly");
+        }
     }
 
     @Override
@@ -205,6 +298,12 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         unregisterReceiver(mUsbReceiver);
         unbindService(usbConnection);
+
+        // stop location updates
+        if (googleApiClient != null  &&  googleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            googleApiClient.disconnect();
+        }
     }
 
     private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
@@ -341,4 +440,101 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+  private ArrayList<String> permissionsToRequest(ArrayList<String> wantedPermissions) {
+    ArrayList<String> result = new ArrayList<>();
+
+    for (String perm : wantedPermissions) {
+      if (!hasPermission(perm)) {
+        result.add(perm);
+      }
+    }
+
+    return result;
+  }
+
+  private boolean hasPermission(String permission) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      return checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    return true;
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+
+    if (googleApiClient != null) {
+      googleApiClient.connect();
+    }
+  }
+
+  private boolean checkPlayServices() {
+    GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+    int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+
+    if (resultCode != ConnectionResult.SUCCESS) {
+      if (apiAvailability.isUserResolvableError(resultCode)) {
+        apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST);
+      } else {
+        finish();
+      }
+
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public void onConnected(@Nullable Bundle bundle) {
+    if (ActivityCompat.checkSelfPermission(this, 
+	                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        &&  ActivityCompat.checkSelfPermission(this, 
+		             Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      return;
+    }
+
+    // Permissions ok, we get last location
+    location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+    if (location != null) {
+      locationTv.setText("Latitude : " + location.getLatitude() + "\nLongitude : " + location.getLongitude());
+    }
+
+    startLocationUpdates();
+  }
+
+  private void startLocationUpdates() {
+    locationRequest = new LocationRequest();
+    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    locationRequest.setInterval(UPDATE_INTERVAL);
+    locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+    if (ActivityCompat.checkSelfPermission(this, 
+	          Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+          &&  ActivityCompat.checkSelfPermission(this, 
+		      Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      Toast.makeText(this, "You need to enable permissions to display location !", Toast.LENGTH_SHORT).show();
+    }
+
+    LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+  }
+
+  @Override
+  public void onConnectionSuspended(int i) {
+  }
+
+  @Override
+  public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+  }
+
+  @Override
+  public void onLocationChanged(Location location) {
+    if (location != null) {
+      locationTv.setText("Latitude : " + location.getLatitude() + "\nLongitude : " + location.getLongitude());
+    }
+  }
+
 }
